793965b73d4cce690431a353cf6ebb276768c0f0
shrestha <happygreat123411@gmail.com>
2025-04-01 13:55:22 +0530

    Added screenshots to assets folder

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../others/header'
+import Header from '../others/Header'
 import TaskListNumber from '../others/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
